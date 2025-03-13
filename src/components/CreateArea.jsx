247d8a0666d4e5dc0edcffc17588061dc6fd4b8d
fountaina <fountainaigbona@gmail.com>
2025-03-13 08:05:30 +0100

    make CreateArea component more efficient and optimized with less react states

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -9,66 +9,59 @@ import Zoom from '@mui/material/Zoom';
 // in the Zoom component, the in prop should be equal to false by default.
 
 function CreateArea({ onAdd }) {
-    // Controlled value state for both title and text input
-    const [newTitleInput, setNewTitleInput] = useState("");
-    const [newTextInput, setNewTextInput] = useState("");
-    const [hideTextArea, setHideTextArea] = useState(true);
-    const [displayAddButton, setDisplayAddButton] = useState(false);
-    const [rowNumber, setRowNumber] = useState("1");
+    //State for adding notes
+    const [noteInput, setNoteInput] = useState({title: "", content: ""});
 
-    const handleTitleChange = (event) => {
-        const titleInputValue = event.target.value;
-        setNewTitleInput(titleInputValue);
-    };
+    // State for text box expansion
+    const [isExpand, setIsExpand] = useState(false);
 
-    const handleTextChange = (event) => {
-        const textInputValue = event.target.value;
-        setNewTextInput(textInputValue);
+    function expand() {
+        setIsExpand(true);
+    }
+
+    const handleChange = (event) => {
+        const {name, value} = event.target;
+        setNoteInput((prevInput) => ({
+            ...prevInput,
+            [name]: value
+        }))
     };
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        console.log(`Title: ${newTitleInput} Text: ${newTextInput}`);
+        console.log(`Title: ${noteInput.title} Text: ${noteInput.content}`);
         
         // Call the onAdd function if needed
         if (onAdd) {
-            onAdd(event, newTitleInput, newTextInput);
+            onAdd(event, noteInput.title, noteInput.content);
         }
 
         // Clear the inputs after submission
-        setNewTitleInput("");
-        setNewTextInput("");
+        setNoteInput({title: "", content: ""});
     };
-
-    const handleTextAreaVisibility = () => {
-        setHideTextArea(false);
-        setDisplayAddButton(true);
-    }
-
-    const handleTextAreaExpansion = () => {
-        setRowNumber("3");
-    }
 
     return (
         <div>
         <form className="create-note" onSubmit={handleSubmit}>
             <input 
-                onChange={handleTitleChange} 
-                value={newTitleInput} 
+                onChange={handleChange} 
+                value={noteInput.title} 
                 name="title" 
                 placeholder="Title" 
-                onClick={handleTextAreaVisibility}
+                onClick={expand}
             />
-            <textarea 
+            {
+                isExpand ? 
+                <textarea 
                 name="content" 
                 placeholder="Take a note..." 
-                rows={rowNumber}
-                value={newTextInput} 
-                onChange={handleTextChange}
-                onClick={handleTextAreaExpansion}
-                hidden={hideTextArea}
-            />
-            <Zoom in={displayAddButton}>
+                rows={noteInput.content.length > 33 ? "3" : null}
+                value={noteInput.content} 
+                onChange={handleChange}
+                />
+                : null
+            }
+            <Zoom in={isExpand}>
                 <Fab type="submit">
                     <AddIcon />
                 </Fab>
