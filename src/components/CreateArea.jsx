2fe4a6dc96aa90bea31836ddb6931c048cca2e16
fountaina <fountainaigbona@gmail.com>
2025-03-13 04:17:53 +0100

    add material Ui zoom and text area  animations

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,9 +1,20 @@
 import React, { useState } from "react";
+import AddIcon from '@mui/icons-material/Add';
+import Fab from '@mui/material/Fab';
+import Zoom from '@mui/material/Zoom';
+
+// text rows should be 1 at default
+// title input should be non existent at default
+// text area should also be non existent at default DONE
+// in the Zoom component, the in prop should be equal to false by default.
 
 function CreateArea({ onAdd }) {
     // Controlled value state for both title and text input
     const [newTitleInput, setNewTitleInput] = useState("");
     const [newTextInput, setNewTextInput] = useState("");
+    const [hideTextArea, setHideTextArea] = useState(true);
+    const [displayAddButton, setDisplayAddButton] = useState(false);
+    const [rowNumber, setRowNumber] = useState("1");
 
     const handleTitleChange = (event) => {
         const titleInputValue = event.target.value;
@@ -29,12 +40,39 @@ function CreateArea({ onAdd }) {
         setNewTextInput("");
     };
 
+    const handleTextAreaVisibility = () => {
+        setHideTextArea(false);
+        setDisplayAddButton(true);
+    }
+
+    const handleTextAreaExpansion = () => {
+        setRowNumber("3");
+    }
+
     return (
         <div>
-        <form onSubmit={handleSubmit}>
-            <input onChange={handleTitleChange} value={newTitleInput} name="title" placeholder="Title" />
-            <textarea name="content" placeholder="Take a note..." rows="3" value={newTextInput} onChange={handleTextChange}/>
-            <button type="submit">Add</button>
+        <form className="create-note" onSubmit={handleSubmit}>
+            <input 
+                onChange={handleTitleChange} 
+                value={newTitleInput} 
+                name="title" 
+                placeholder="Title" 
+                onClick={handleTextAreaVisibility}
+            />
+            <textarea 
+                name="content" 
+                placeholder="Take a note..." 
+                rows={rowNumber}
+                value={newTextInput} 
+                onChange={handleTextChange}
+                onClick={handleTextAreaExpansion}
+                hidden={hideTextArea}
+            />
+            <Zoom in={displayAddButton}>
+                <Fab type="submit">
+                    <AddIcon />
+                </Fab>
+            </Zoom>
         </form>
         </div>
     );
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import DeleteIcon from '@mui/icons-material/Delete';
 
 const Note = ({ id, title, content, onDelete}) => {
   
@@ -11,8 +12,9 @@ const Note = ({ id, title, content, onDelete}) => {
     <div className="note">
       <h1>{title}</h1>
       <p>{content}</p>
-      <button onClick={handleClick}><img src="/images/delete2.svg" alt="delete" /></button>
+      {/* <button onClick={handleClick}><img src="/images/delete2.svg" alt="delete" /></button> */}
       {/* <img onClick={handleClick} src="/images/delete2.svg" alt="delete" /> */}
+      <button onClick={handleClick}><DeleteIcon /></button>
     </div>
   );
 };
